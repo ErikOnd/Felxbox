cc2c855800a88ea242bb4a153f4c6156df83a8be
Erik <erik.ondra@yahoo.com>
2022-12-26 11:21:25 +0100

    fixed some flex stuff, works perfectly now

--- script.js
+++ script.js
@@ -1,23 +1,33 @@
 let containerNode = document.getElementsByClassName('container')[0]
 
-let num = 1
-let counter = 1;
-while (num <= 12) {
-    let divContainer = document.createElement('div')
-    divContainer.classList.add('divContainer')
-    containerNode.appendChild(divContainer)
 
-    for (let index = num; index > 0; index--) {
-        let divNode = document.createElement('div')
-        divNode.classList.add('divStyle')
-        let height = 95 / counter;
-        height = height + '%'
-        divNode.style.height = height;
-        divContainer.appendChild(divNode)
+
+
+
+function createDivs(amount) {
+    let num = 1
+    let counter = 1;
+    while (num <= amount) {
+        let divContainer = document.createElement('div')
+        divContainer.classList.add('divContainer')
+        containerNode.appendChild(divContainer)
+
+        for (let index = num; index > 0; index--) {
+            let divNode = document.createElement('div')
+            //divNode.style.width = 'auto'
+            divNode.classList.add('divStyle')
+            let height = 100 / counter;
+            height = height + '%'
+            divNode.style.height = height;
+            divContainer.appendChild(divNode)
+        }
+        num++
+        counter++;
     }
-    num++
-    counter++;
 }
+
+
+createDivs(12)
 
 
 
